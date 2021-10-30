44261a4dce48934e04667c94c00548691d16c2b1
arivermouth <joensuu.arttu@gmail.com>
2021-10-30 12:36:23 +0300

    calculator

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, TextInput, Text } from 'react-native';
 
 export default function App() {
+  const [number1, onChangeNumber1] = React.useState(null);
+  const [number2, onChangeNumber2] = React.useState(null);
+  const [number3, onChangeNumber3] = React.useState(null);
+  
+  const sum = () => {
+    var n1 = parseInt(number1);
+    var n2 = parseInt(number2);
+    var s = n1+n2;
+    onChangeNumber3(s)
+  }
+  const subtraction = () => {
+    var n1 = parseInt(number1);
+    var n2 = parseInt(number2);
+    var s = n1-n2;
+    onChangeNumber3(s)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Mobiiliohjelmoinnin kurssi</Text>
-      <StatusBar style="auto" />
+      <Text>
+        Result: {number3}
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={number1 => onChangeNumber1(number1)}
+        value={number1}
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={number2 => onChangeNumber2(number2)}
+        value={number2}
+        keyboardType="numeric"
+      />
+      <View style={styles.button}>
+        <Button 
+          style={styles.button}
+          onPress={sum} 
+          title="+"
+        />
+        <Button 
+          style={styles.button}
+          onPress={subtraction} 
+          title="-"
+        />
+      </View>
+      
     </View>
   );
 }
@@ -16,6 +58,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  input: {
+    width: 200,
+    borderWidth: 1,
+    margin: 5,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: 30,
+    width: 100,
+  }
+  
 });
