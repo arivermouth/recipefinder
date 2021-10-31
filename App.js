9e3585f94692204f50040410d42a3e3c01da51ef
arivermouth <joensuu.arttu@gmail.com>
2021-11-01 01:56:49 +0200

    valmis

--- App.js
+++ App.js
@@ -1,54 +1,46 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, TextInput, Text } from 'react-native';
+import { StyleSheet, View, Button, TextInput, Text, FlatList } from 'react-native';
 
 export default function App() {
-  const [number1, onChangeNumber1] = React.useState(null);
-  const [number2, onChangeNumber2] = React.useState(null);
-  const [number3, onChangeNumber3] = React.useState(null);
-  
-  const sum = () => {
-    var n1 = parseInt(number1);
-    var n2 = parseInt(number2);
-    var s = n1+n2;
-    onChangeNumber3(s)
-  }
-  const subtraction = () => {
-    var n1 = parseInt(number1);
-    var n2 = parseInt(number2);
-    var s = n1-n2;
-    onChangeNumber3(s)
+  const[text, setText] = useState('');
+  const[data, setData] = useState([]);
+
+  const add = () => {
+    setData([...data, {key:text }]);
+    setText('');
   }
 
   return (
     <View style={styles.container}>
-      <Text>
-        Result: {number3}
-      </Text>
       <TextInput
         style={styles.input}
-        onChangeText={number1 => onChangeNumber1(number1)}
-        value={number1}
-        keyboardType="numeric"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={number2 => onChangeNumber2(number2)}
-        value={number2}
-        keyboardType="numeric"
+        onChangeText={text => setText(text)}
+        value={text}
+        keyboardType="default"
       />
       <View style={styles.button}>
         <Button 
           style={styles.button}
-          onPress={sum} 
-          title="+"
+          onPress={add} 
+          title="ADD"
         />
         <Button 
           style={styles.button}
-          onPress={subtraction} 
-          title="-"
+          onPress={data => setData([])} 
+          title="CLEAR"
         />
       </View>
+      <Text style={{color: 'blue', fontWeight: 'bold'}}>
+        Shopping List
+      </Text>
       
+      <FlatList
+        data={data}
+        renderItem={({item}) =>
+          <Text>
+            {item.key}
+          </Text>}
+      />
     </View>
   );
 }
@@ -58,7 +50,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    padding: 20
   },
   input: {
     width: 200,
@@ -72,6 +65,6 @@ const styles = StyleSheet.create({
     margin: 30,
     width: 100,
   }
-  
 });
+
 
