a0487fb5016d379203db07f5b12c0748f22104f2
arivermouth <joensuu.arttu@gmail.com>
2021-11-09 20:43:42 +0200

    first commit

--- App.js
+++ App.js
@@ -1,18 +1,61 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import CalculatorScreen from './CalculatorScreen';
-import HistoryScreen from './HistoryScreen';
-
-const Stack = createStackNavigator();
+import React, { useState } from 'react';
+import { Alert, StyleSheet, Text, View, Button, TextInput, FlatList, StatusBar, Image } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  const getRepositories = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+    .then(response => response.json())
+    .then(responseJson => setRepositories(responseJson.meals))
+    .catch(error => { 
+        Alert.alert('Error', error); 
+    });    
+  }
+  
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Calculator" component={CalculatorScreen} />
-        <Stack.Screen name="History" component={HistoryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <StatusBar hidden={true} />
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) => 
+          <View>
+            <Text style={{fontSize: 18, fontWeight: "bold"}}>{item.strMeal}</Text>
+            <Image style={styles.image} source={{uri: `${item.strMealThumb}`}} />
+          </View>}
+        data={repositories} 
+        ItemSeparatorComponent={listSeparator} /> 
+      <TextInput style={{fontSize: 18, width: 200}} placeholder='keyword' 
+        onChangeText={text => setKeyword(text)} />
+      <Button title="Find" onPress={getRepositories} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image : {
+    width: 100,
+    height: 100
+  }
+});
